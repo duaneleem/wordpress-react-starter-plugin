3107a265e1b0f13bc3d4c88b7aa0e7f0c64ff0df
Duane Leem <Duane@LeemFamily.com>
2018-07-01 12:43:56 -0700

    Initial functions.php

---
+++ wordpress-react-starter-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://duaneleem.com
+ * @since             1.0.0
+ * @package           DL_WP_React_Starter
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WordPress React Starter Plugin
+ * Plugin URI:        https://github.com/duaneleem/wordpress-react-starter-plugin
+ * Description:       Turn /react route to a React application.
+ * Version:           1.0.0
+ * Author:            Duane Leem
+ * Author URI:        https://duaneleem.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       dl-wp-react-starter
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) { die; }
+
